b6cf7908c424e1c18565d03079f463013b1dbc59
ryanhihgj-cyber <ryanhihgj@gmail.com>
2025-09-22 10:30:58 -0600

    Update server.js

--- server.js
+++ server.js
@@ -64,4 +64,3 @@ app.get('/', (req, res) => res.send('Slack interactivity server is running!'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
- console.log(`Server running on port ${PORT}`));
